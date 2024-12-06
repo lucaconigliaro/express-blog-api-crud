0720c5e00c87f69b9304492d69653f26c6e825cd
lucaconigliaro <lucaconigliaro1@gmail.com>
2024-12-06 13:23:06 +0100

    Handle Error

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require(`express`); 
-const postsRouters = require("./routers/posts"); // Importiamo il router che conterrà le rotte relative alla gestione post
+const postsRouters = require("./routers/posts");
+const handleError = require(`./middlewares/handleError`);
 
 const app = express();
 const port = 3001; 
@@ -15,10 +16,15 @@ app.use(`/posts`, postsRouters);
 
 // Creiamo una rotta per la homepage (`/`) che risponde con un messaggio JSON.
 app.get(`/`, (req, res) => {
+    ops(); //funzione che non esiste e da l'errore
+    console.log("La rotta è stata chiamata");
     res.json({
         message: "Server del mio blog"
     })
 });
+
+// Dopo tutte le rotte inseriamo il middleware che gestisce l'errore
+app.use(handleError);
 
 // Avviamo il server sull'indirizzo localhost alla porta specificata (3001).
 app.listen(port, () => {
