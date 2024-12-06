9f559b9b8b1d77ef19e0cd489999ed49dace1032
lucaconigliaro <lucaconigliaro1@gmail.com>
2024-12-06 13:14:28 +0100

    Middleware checkPost

---
+++ middlewares/checkPostExists.js
@@ -0,0 +1,18 @@
+const posts = require(`../data/data`);
+
+const checkPostExists = (req, res, next) => {
+    console.log("Controllo se il post esiste");
+    const postId = parseInt(req.params.id);
+    const post = posts.find((curPost) => curPost.id === postId);
+    if (post !== undefined) {
+        next ();
+    } else {
+        res.statusCode = 404;
+        res.json({
+            error: true,
+            message: "Post non trovato"
+        });
+    }
+};
+
+module.exports = checkPostExists;
--- routers/posts.js
+++ routers/posts.js
@@ -1,20 +1,21 @@
 const express = require(`express`);
 const postsController = require(`../controllers/postsController`);
+const checkPostExists = require(`../middlewares/checkPostExists`);
 const router = express.Router();
 
 // Index
 router.get(`/`, postsController.index);
 
 // Show
-router.get(`/:id`, postsController.show);
+router.get(`/:id`, checkPostExists, postsController.show); //registro il middleware per singola rotta (ad esempio quelle che hanno "/:id")
 
 // Create
 router.post("", postsController.store);
 
 // Update 
-router.put(`/:id`, postsController.update );
+router.put(`/:id`, checkPostExists, postsController.update );
 
 // Delete
-router.delete(`/:id`, postsController.destroy);
+router.delete(`/:id`, checkPostExists, postsController.destroy);
 
 module.exports = router;
